506f045050b4efc5c466d054a2a081560df8d62d
antvega <antvega627@gmail.com>
2023-12-11 19:18:57 -0800

    Increasing amount of guesses.

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  // Actions a user can take:
+  // Reveal a card
+  // Reset guess
+  // Reset game
+  switch (action.type) {
+    case "make-a-guess":
+      return { ...state, numberOfGuess: state.numberOfGuess + 1 };
+    case "reset-guess":
+      return state;
+    case "reset-game":
+      return state;
+  }
+  return state;
+}
+function getInitialState() {
+  return {
+    board: ["a", "b", "a", "b"],
+    revealed: [],
+    numberOfGuess: 0,
+  };
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, undefined, getInitialState);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{JSON.stringify(state, null, 2)}</Text>
+      <Button
+        title="Guess"
+        onPress={() => {
+          dispatch({ type: "make-a-guess" });
+        }}
+      ></Button>
     </View>
   );
 }
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
