0ef7da6b768a2f242310720b39b227e02d2e301d
antvega <antvega627@gmail.com>
2023-12-12 19:06:16 -0800

    Randomize board.

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { useReducer } from "react";
 
+const SYMBOLS = ["a", "b", "c"];
+
 function reducer(state, action) {
   // Actions a user can take:
   // Reveal a card
@@ -13,20 +15,33 @@ function reducer(state, action) {
     case "reset-guess":
       return state;
     case "reset-game":
-      return state;
+      return createNewState;
   }
   return state;
 }
-function getInitialState() {
+function createNewState() {
+  const board = [...SYMBOLS, ...SYMBOLS];
+  shuffle(board);
   return {
-    board: ["a", "b", "a", "b"],
+    board,
     revealed: [],
     numberOfGuess: 0,
   };
 }
 
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; --i) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    swap(arr, randomIndex, i);
+  }
+}
+function swap(arr, randomIndex, i) {
+  const randomValue = arr[randomIndex];
+  arr[randomIndex] = arr[i];
+  arr[i] = randomValue;
+}
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, undefined, getInitialState);
+  const [state, dispatch] = useReducer(reducer, undefined, createNewState);
 
   return (
     <View style={styles.container}>
